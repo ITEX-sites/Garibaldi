bd660e51e29bd0c9ba5d1cdaf6eb26992a43e789
Courtney Collins <courtneygrace125@gmail.com>
2026-05-04 16:57:59 -0700

    clean up folders

--- Trampling_spp_richness/scripts/teagan_spp_merging.R
+++
@@ -1,6 +0,0 @@
- names(garibaldi_trampling_species_matrix)
-library("tidyverse")
-gtsm %>% select(order(colnames(gtsm))) -> gtsm_alphabetical
-gtsm <- garibaldi_trampling_species_matrix
-gtsm_alphabetical <- rename(gtsm_alphabetical, Castilleja.parvifolia = "Castilleja_hybrid_PR18")
-gtsm_alphabetical <- rename(gtsm_alphabetical, Carex.1 = "Carex_1_BT9")
